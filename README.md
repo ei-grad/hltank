13d521423e0e657d3aaca2431cbbf6a8a3b88c31
Andrew Grigorev <andrew@ei-grad.ru>
2017-08-20 19:58:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+hltank
+======
+
+This is a testing tool I wrote for Highload Cup 2017/08.
---
+++ main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"flag"
+	"io"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	errEmptyInt               = errors.New("empty integer")
+	errUnexpectedFirstChar    = errors.New("unexpected first char found. Expecting 0-9")
+	errUnexpectedTrailingChar = errors.New("unexpected traling char found. Expecting 0-9")
+	errTooLongInt             = errors.New("too long int")
+)
+
+var maxIntChars = 10
+
+func ParseInt(b []byte) (int, error) {
+	n := len(b)
+	if n == 0 {
+		return 0, errEmptyInt
+	}
+	var v int
+	for i := 0; i < n; i++ {
+		c := b[i]
+		if c == ' ' {
+			return v, nil
+		}
+		k := c - '0'
+		if k > 9 {
+			if i == 0 {
+				return 0, errUnexpectedFirstChar
+			}
+			return 0, errUnexpectedTrailingChar
+		}
+		if i >= maxIntChars {
+			return 0, errTooLongInt
+		}
+		v = 10*v + int(k)
+	}
+	return v, nil
+}
+
+var pool = &SizedBufferPool{}
+
+func main() {
+
+	ammoFileName := flag.String("ammo", "", ".ammo file name")
+	target := flag.String("target", "127.0.0.1:80", "target")
+	nWorkers := flag.Int("w", 32, "number of connections")
+
+	flag.Parse()
+
+	f, err := os.Open(*ammoFileName)
+	if err != nil {
+		log.Fatal("can't open ammo file:", err)
+	}
+	defer f.Close()
+
+	for {
+		r := bufio.NewReader(f)
+
+		requests := make(chan []byte)
+		defer close(requests)
+
+		for i := 0; i < *nWorkers; i++ {
+			go worker(*target, requests)
+		}
+
+		for {
+			bulletHeader, err := r.ReadSlice('\n')
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatal("can't read bullet header:", err)
+			}
+			bulletLen, err := ParseInt(bulletHeader)
+			if err != nil {
+				log.Fatal("can't parse bullet length:", err)
+			}
+			buf := pool.Get(bulletLen)
+			_, err = io.ReadFull(r, buf)
+			if err != nil {
+				log.Fatal("can't read request from file:", err)
+			}
+			requests <- buf
+		}
+		f.Seek(0, 0)
+	}
+
+}
+
+type SizedBufferPool struct {
+	pools []*sync.Pool
+	mu    sync.RWMutex
+}
+
+func (p *SizedBufferPool) Get(size int) []byte {
+	var (
+		i = 0
+		j = 4096
+	)
+	for {
+		if i >= len(p.pools) {
+			p.mu.Lock()
+			if i >= len(p.pools) {
+				p.pools = append(p.pools, &sync.Pool{})
+			}
+			p.mu.Unlock()
+		}
+		if j >= size {
+			p.mu.RLock()
+			ret := p.pools[i].Get()
+			p.mu.RUnlock()
+			if ret == nil {
+				ret = make([]byte, j)
+			}
+			return ret.([]byte)[:size]
+		}
+		i++
+		j *= 2
+	}
+}
+
+func (p *SizedBufferPool) Put(b []byte) {
+	var (
+		i = 0
+		j = 64
+	)
+	for {
+		if i >= len(p.pools) {
+			p.pools = append(p.pools, &sync.Pool{})
+		}
+		if j == cap(b) {
+			p.pools[i].Put(b)
+		}
+		i++
+		j *= 2
+	}
+}
+
+func worker(target string, requests chan []byte) {
+
+	var (
+		client    = &fasthttp.HostClient{Addr: target}
+		resp      fasthttp.Response
+		req       fasthttp.Request
+		reqReader = bufio.NewReader(bytes.NewReader([]byte{}))
+	)
+
+	for b := range requests {
+		// XXX: we need MOAR bytes.NewReader's! SIC!
+		// TODO: write custom reader
+		reqReader.Reset(bytes.NewReader(b))
+		err := req.Read(reqReader)
+		pool.Put(b)
+		if err != nil {
+			log.Fatal("can't read request from buffer:", err)
+		}
+		err = client.Do(&req, &resp)
+		if err != nil {
+			log.Fatal("request failed:", err)
+		}
+		req.Reset()
+		resp.Reset()
+	}
+}
